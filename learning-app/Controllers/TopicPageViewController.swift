c30c9d8fbdbae2fae323fc905a00a84ca189b659
Devon <devonmaguire@Devons-MacBook-Pro.local>
2020-02-25 12:11:57 -0500

    Initial topic page by Beth and Devon :)

--- learning-app/Controllers/TopicPageViewController.swift
+++ learning-app/Controllers/TopicPageViewController.swift
@@ -10,10 +10,41 @@ import UIKit
 
 class TopicPageViewController: UIViewController {
 
-    let firstTopic = TopicModel(name: "iOS", picture: "Apple_gray_logo", description: "The dopest logo", links: ["www.apple.com"], people: ["Yooo"], events: [1])
+    @IBOutlet weak var topicNameLabel: UILabel!
+    @IBOutlet weak var topicImageView: UIImageView!
+    @IBOutlet weak var topicDescriptionLabel: UILabel!
+    
+    @IBOutlet weak var topicResourcesLabel: UILabel!
+    @IBOutlet weak var topicResourceLinksLabel: UILabel!
+    
+    @IBOutlet weak var topicInterestLabel: UILabel!
+    
+    @IBOutlet weak var topicEventsHeadingLabel: UILabel!
+    @IBOutlet weak var topicEventsLabel: UILabel!
+    
+    
+    
+    
+    let firstTopic = TopicModel(name: "iOS", picture: "Apple_gray_logo", description: "The dopest logo", links: ["www.apple.com"], people: [], events: [1])
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        topicNameLabel.text = firstTopic.name
+        topicImageView.image = UIImage(named: firstTopic.picture)
+        topicDescriptionLabel.text = firstTopic.description
+        // need to do links and events still
+        let people = firstTopic.people
+        if (people.count == 0) {
+            topicInterestLabel.text = "You are the first person interested in this topic"
+        } else if (people.count == 1) {
+            topicInterestLabel.text = people[0] + " is also interested in this topic"
+        } else if (people.count == 2) {
+            topicInterestLabel.text = people[0] + " and " + people[1] + " are also interested in this topic"
+        } else {
+            let count = String(people.count - 2)
+            topicInterestLabel.text = people[0] + ", " + people[1] + " and " + count + " others are interested in this topic"
+        }
+        
     }
 }
